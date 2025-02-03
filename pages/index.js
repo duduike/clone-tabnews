276b9c884c8caeecbcce302eed677dbaf9a0f81a
Eduardo Pina <101907618+duduike@users.noreply.github.com>
2025-02-03 13:49:42 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-function Home () {
-
-    return (
-        <>
-            <h1>Oi lindinha, da uma risadinha.</h1>
-        </>
-    )   
+function Home() {
+  return (
+    <>
+      <h1>Oi lindinha, da uma risadinha.</h1>
+    </>
+  );
 }
 
 export default Home;
